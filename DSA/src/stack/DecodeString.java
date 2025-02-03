b68bb391803c0fe98f2e4c5e00c76baf65d2b834
mohammedanasfl <nanass21072001@gmail.com>
2025-02-03 16:02:03 +0530

    decodeString logic added

--- DSA/src/stack/DecodeString.java
+++ DSA/src/stack/DecodeString.java
@@ -1,14 +1,39 @@
 package stack;
 
+import java.util.Stack;
+
 public class DecodeString {
 
     public static String decodeString(String s) {
-        return "";
+        Stack<Integer>numStack=new Stack<>();
+        Stack<StringBuilder>strStack=new Stack<>();
+        int currNum=0;
+        StringBuilder currStr=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(Character.isDigit(c)){
+               currNum=currNum*10+(c-'0');
+            } else if (c == '[') {
+                numStack.push(currNum);
+                strStack.push(currStr);
+                currNum=0;
+                currStr=new StringBuilder();
+            } else if (c ==']') {
+                int count = numStack.pop();
+                StringBuilder repeated = strStack.pop();
+                for (int i = 0; i < count; i++) {
+                    repeated.append(currStr);
+                }
+                currStr = repeated;
+            }else {
+                currStr.append(c);
+            }
+        }
+        return currStr.toString();
 
     }
 
     public static void main(String[]args){
-        System.out.print("Size = "+decodeString("3[a2[c]]"));
+        System.out.print("Size = "+decodeString("100[leetcode]"));
     }
 }
 
