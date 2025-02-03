8709f5e4b269eca6561e7b6bbaa5f91a1b9b1bc2
mohammedanasfl <nanass21072001@gmail.com>
2025-02-03 13:44:30 +0530

    initial check

---
+++ DSA/src/stack/DecodeString.java
@@ -0,0 +1,14 @@
+package stack;
+
+public class DecodeString {
+
+    public static String decodeString(String s) {
+        return "";
+
+    }
+
+    public static void main(String[]args){
+        System.out.print("Size = "+decodeString("3[a2[c]]"));
+    }
+}
+
